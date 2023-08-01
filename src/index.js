443c6f190ca23872da3a9b625946ef118c056f1e
Evgeniy <durasell96ms@gmail.com>
2023-08-01 16:53:57 +0300

    init: start Reverse_Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+    
+    let result = 0;
+    let a = Math.abs(n);
+    while (a > 0) {
+      result = result * 10 + a % 10;
+      a = Math.floor(a / 10);
+    }
   
+    if (n < 0) {
+        // result = result * (-1);
+    } 
+    return result;
 }
